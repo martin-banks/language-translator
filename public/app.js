487ae5cea9808dc9d58ad705fc1fa206f17fd3be
Martin Banks <anotherbanksy@gmail.com>
2017-12-11 01:14:43 +1100

    adds voice recognition translation

--- public/app.js
+++ public/app.js
@@ -1,8 +1,67 @@
 
-const container = document.querySelector('pre')
+// const translateButton = document.querySelector('.translateButton')
+// const message = () => document.querySelector('input').value
+const languageToList = document.querySelector('.languages__to')
+const translation = document.querySelector('.translation')
+const listen = document.querySelector('.listen')
+const languageTo = () => languageToList.querySelector('select').value
 const socket = io()
-console.log(socket)
-socket.on('languages', data => container.innerHTML = JSON.stringify(data.languages, 'utf8', 2))
-// button.addEventListener('click', e => {
-// 	socket.emit('send', { msg: 'Hello world, please translate this into a foriegn language' })
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
+var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
+var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
+
+const synth = window.speechSynthesis
+
+
+let speech = null
+let voices = []
+
+listen.addEventListener('click', e => {
+	speech = new SpeechRecognition()
+	speech.start()
+	speech.addEventListener('result', e => {
+		const msg = e.results[0][0].transcript
+		console.log(msg)
+		socket.emit('translate', { msg, to: languageTo() })
+	})
+})
+
+
+function createLanguageList({ data, type }) {
+	const list = items => `<select name="languages" id="kanguages">${items}</select>`
+	const item = value => `<option value="${value.code}">${value.name}</option>`
+	const output = list(data.map(d => item(d)).join(''))
+	return ` Choose language ${type}: ${output}`
+}
+
+function setTranslation(data) {
+	const { msg } = data
+	const talk = new SpeechSynthesisUtterance(msg)
+	voices = synth.getVoices()
+	const voiceToUse = voices
+		.filter(v => v.lang.split('-')[0] === languageTo())[0]
+		// .forEach(v => console.log(v.lang))
+	talk.voice = voiceToUse
+	synth.speak(talk)
+	// let suffix = ''
+	// const suffixTemplate = value => `<p>${value}</p>`
+	// if (msg.toLowerCase() === message().toLowerCase()) {
+	// 	suffix = suffixTemplate('Hmm, looks like it\'s the same, did you set the correct language?')
+	// }
+	translation.innerHTML = `${msg}`
+}
+
+
+
+
+// translateButton.addEventListener('click', e => {
+// 	setTranslation({ msg: 'Waiting for translation' })
+// 	socket.emit('translate', { msg: message(), to: languageTo() })
 // })
+
+socket.on('languages', data => {
+	languageToList.innerHTML = createLanguageList({ data: data.languages, type: 'to' })
+	languageToList.querySelector('select').value = 'en'
+})
+socket.on('translation', setTranslation)
+
--- start.js
+++ start.js
@@ -3,45 +3,37 @@ require('dotenv').config()
 // const google = require('googleapis')
 const Translate = require('@google-cloud/translate')
 
-
-
-
 var express = require('express')
 const app = express()
 var http = require('http').Server(app)
+// Create socket.io instance
+// We'll use this to send content 
+// to translate and translated responses back to client
 var io = require('socket.io')(http)
 const path = require('path')
 
 const projectId = process.env.PROJECT
 // const key = process.env.TRANSLATE
 const keyFilename = process.env.KEYFILE
-
-// app.get('/', function(req, res){
-//   res.send('<h1>Hello world</h1>');
-// });
-
-http.listen(3000, function(){
-  console.log('listening on *:3001');
+const port = 3000
+http.listen(port, function(){
+  console.log(`listening on *: ${port}`);
 });
 
-const translate = new Translate({
-	keyFilename,
-	projectId,
-})
+// Create new translate instance
+// This is what we'll call later to access translate api
+const translate = new Translate({ keyFilename, projectId })
 
-
-
-// Where are the view tempaltes stored
+// Where are the view tempaltes stored ...
 app.set('views', path.join(__dirname, 'views'))
-// The view engine to render the templates
+// The view engine to render the templates ...
 app.set('view engine', 'pug')
 
-// Static files hosted from here
+// Static files (scripts and styles ) hosted from here
 app.use('/public', express.static(path.join(__dirname, '/public')))
 
 // Variables to pass into templates for all route requests
 app.use((req, res, next) => {
-	res.locals.menu = ['first', 'second']
 	next()
 })
 
@@ -49,26 +41,35 @@ app.use((req, res, next) => {
 app.get('/', (req, res) => res.render('layout'))
 
 
+// Start socket.io connection
 io.on('connection', function (socket) {
+	// when a user connects, get a list of languages available to translate ...
 	translate
 		.getLanguages()
 		.then(data => {
+			// ... and return it back to client
 			socket.emit('languages', { languages: data[0] })
 		})
 		.catch(console.error)
 	
-  socket.on('send', data => {
+	// Start listening for 'send' events
+	// This event is used by the client to send messages to translate
+  socket.on('translate', data => {
+  	// send the message to the tanslate api
   	translate
-			.translate(data.msg, 'fr')
+			.translate(data.msg, data.to)
 			.then(res => {
 				console.log(res[0])
-		  	socket.emit('reply', { msg: res[0] })
+				// return the translated 
+		  	socket.emit('translation', { msg: res[0] })
 			})
 			.catch(err => {
 				console.error('ERROR!!', err)
 			})
   })
 })
+
+
 
 
 // function handler (req, res) {
